5af653aac3a2321f9defb74c2be5e4786d105a1a
Panuwat Palipote <sthunder@hotmail.com>
2013-05-05 22:15:37 +0700

    add 3 tests (novote,createLink,vote)

--- test/unit/linklike/LinkControllerTests.groovy
+++ test/unit/linklike/LinkControllerTests.groovy
@@ -9,9 +9,59 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(LinkController)
+@Mock([Link])
 class LinkControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testNoVote() {
+
+       params.title = 'wat'
+       params.url = 'wat'
+       controller.create() 
+
+        assert Link.get(1).votenumber == 0
+    } 
+
+    void testLink(){
+
+     	//params.title = 'wat'
+        //params.url = 'wat'
+        //controller.create() 
+
+        //assert Link.count == 1
+
+        //controller.index()
+
+        params.title = 'wat1'
+        params.url = 'wat2'
+       controller.create()
+       assert Link.get(1).title == 'wat1'
+       assert Link.count == 1 
     }
+    void testVote()
+    {
+    	mockDomain(Link,
+    		[ [title:'a1',url:'b1'],
+    		  [title:'a2',url:'b2'],
+    		  [title:'a3',url:'b3'] ])
+    	
+    	controller.vote('a1','b1')
+    	assert Link.get(1).votenumber == 1
+      assert Link.get(2).votenumber == 0
+      assert Link.get(3).votenumber == 0
+
+      controller.vote('a1','b1')
+      controller.vote('a2','b2')
+      assert Link.get(1).votenumber == 2
+      assert Link.get(2).votenumber == 1
+      assert Link.get(3).votenumber == 0
+
+      controller.vote('a1','b1')
+      controller.vote('a2','b2')
+      controller.vote('a3','b3')
+      assert Link.get(1).votenumber == 3
+      assert Link.get(2).votenumber == 2
+      assert Link.get(3).votenumber == 1
+
+    }
+
 }
